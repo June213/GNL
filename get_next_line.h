938b33cd328263c1986de270ea2c45b8f62090f0
jsalaber <jsalaber@student.42urduliz.com>
2024-01-15 13:43:48 +0100

    archivos vacios

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsalaber <jsalaber@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 09:07:12 by jsalaber          #+#    #+#             */
+/*   Updated: 2024/01/15 13:02:24 by jsalaber         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# inlcude <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
